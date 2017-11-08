07328401c4168a19ab60ac800bf898c9a96d533b
PJunhyuk-DT <jgravity@naver.com>
2017-11-09 03:22:42 +0900

    Remove display parts and add info printing parts

--- deep_sort_app.py
+++ deep_sort_app.py
@@ -199,11 +199,16 @@ def run(sequence_dir, detection_file, output_file, min_confidence,
                 frame_idx, track.track_id, bbox[0], bbox[1], bbox[2], bbox[3]])
 
     # Run tracker.
-    if display:
-        visualizer = visualization.Visualization(seq_info, update_ms=5)
-    else:
-        visualizer = visualization.NoVisualization(seq_info)
-    visualizer.run(frame_callback)
+    # if display:
+    #     visualizer = visualization.Visualization(seq_info, update_ms=5)
+    # else:
+    #     visualizer = visualization.NoVisualization(seq_info)
+    # visualizer.run(frame_callback)
+
+    print("seq_info")
+    print(seq_info)
+    print("frame_callback")
+    print(frame_callback)
 
     # Store results.
     f = open(output_file, 'w')
